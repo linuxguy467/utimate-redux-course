1b99abc0cabfb91e3c3c49d319237dc542590ce1
Matthew Hemingway <dpplrdr435@gmail.com>
2020-07-12 01:17:21 -0400

    Added redux toolkit Refactor: configureStore to use from redux toolkit

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,6 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit';
 import reducer from './bugs';
 
-export default function configureStore() {
-  const store = createStore(reducer, devToolsEnhancer({ trace: true }));
-  return store;
+export default function () {
+  return configureStore({ reducer });
 }
