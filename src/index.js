80db8c72da2abd38960babb79a622a0bdf1456e4
Matthew Hemingway <dpplrdr435@gmail.com>
2020-07-12 01:53:28 -0400

    Refactor: actions and reducer using toolkit

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ store.subscribe(() => {
   console.log('Store changed!');
 });
 
-store.dispatch(actions.bugAdded('Bug 1'));
-store.dispatch(actions.bugAdded('Bug 2'));
-store.dispatch(actions.bugAdded('Bug 3'));
-store.dispatch(actions.bugResolved(1));
+store.dispatch(actions.bugAdded({ description: 'Bug 1' }));
+store.dispatch(actions.bugAdded({ description: 'Bug 2' }));
+store.dispatch(actions.bugAdded({ description: 'Bug 3' }));
+store.dispatch(actions.bugResolved({ id: 1 }));
 
 console.log(store.getState());
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,51 +1,26 @@
-// Action Types
-const BUG_ADDED = 'bugAdded';
-const BUG_REMOVED = 'bugRemoved';
-const BUG_RESOLVED = 'bugResolved';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 // Action creators
-export const bugAdded = (description) => ({
-  type: BUG_ADDED,
-  payload: {
-    description,
-  },
-});
-
-export const bugRemoved = (id) => ({
-  type: BUG_REMOVED,
-  payload: {
-    id,
-  },
-});
-
-export const bugResolved = (id) => ({
-  type: BUG_RESOLVED,
-  payload: {
-    id,
-  },
-});
+export const bugAdded = createAction('bugAdded');
+export const bugRemoved = createAction('bugRemoved');
+export const bugResolved = createAction('bugResolved');
 
 // Reducer
 let lastId = 0;
 
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case BUG_ADDED:
-      return [
-        ...state,
-        {
-          id: ++lastId,
-          description: action.payload.description,
-          resolved: false,
-        },
-      ];
-    case BUG_REMOVED:
-      return state.filter((bug) => bug.id !== action.payload.id);
-    case BUG_RESOLVED:
-      return state.map((bug) =>
-        bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
-      );
-    default:
-      return state;
-  }
-}
+export default createReducer([], {
+  [bugAdded.type]: (bugs, action) => {
+    bugs.push({
+      id: ++lastId,
+      description: action.payload.description,
+      resolved: false,
+    });
+  },
+  [bugResolved.type]: (bugs, action) => {
+    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+    bugs[index].resolved = true;
+  },
+  [bugRemoved.type]: (bugs, action) => {
+    bugs.filter((bug) => bug.id !== action.payload.id);
+  },
+});
